6df494f16d4d692876716f75f9ea00fadf9bffc1
Dilshan Mindika <maxdilshan666@gmail.com>
2024-09-02 17:09:49 +0530

    first commit

---
+++ DASHTAP/src/main/java/com/dashtap/DASHTAP/DashtapApplication.java
@@ -0,0 +1,13 @@
+package com.dashtap.DASHTAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashtapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashtapApplication.class, args);
+	}
+
+}
